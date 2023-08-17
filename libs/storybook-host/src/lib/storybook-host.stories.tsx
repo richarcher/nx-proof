b6fc8be27d281df07f7258f08c45c2533ad79cf4
richarcher <richarcher@users.noreply.github.com>
2023-08-17 17:48:38 +0200

    generate storybook configuration for storybook-host lib

---
+++ libs/storybook-host/src/lib/storybook-host.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { StorybookHost } from './storybook-host';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<typeof StorybookHost> = {
+  component: StorybookHost,
+  title: 'StorybookHost',
+};
+export default meta;
+type Story = StoryObj<typeof StorybookHost>;
+
+export const Primary = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Welcome to StorybookHost!/gi)).toBeTruthy();
+  },
+};
