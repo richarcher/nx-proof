2dc6c60ecaa8b6203456c89224df811151030590
richarcher <richarcher@users.noreply.github.com>
2023-08-18 08:42:59 +0200

    compose the present chromatic storybooks under a single storybook
    One storybook instance to rule them all, One storybook instance to find them
    One storybook instacne to bring them all, and in the darkness bind them

--- libs/storybook-host/.storybook/main.ts
+++ libs/storybook-host/.storybook/main.ts
@@ -2,6 +2,28 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  refs: {
+    pinata: {
+      title: 'Piñata',
+      url: 'https://master--621e37abc7a9e4004aa7ba12.chromatic.com/',
+    },
+    'spa-company-experience': {
+      title: 'SPA Company Experience',
+      url: 'https://master--6369c1f4f8eeb52699a5a947.chromatic.com/',
+    },
+    'spa-candidate-experience': {
+      title: 'SPA Candidate Experience',
+      url: 'https://master--6384bb30fb338537ff9fd667.chromatic.com/',
+    },
+    'spa-operations-experience': {
+      title: 'SPA Operations Experience',
+      url: 'https://master--64526c08d9156df324b7e423.chromatic.com/',
+    },
+    zenicon: {
+      title: 'Zenicon',
+      url: 'https://master--6401a1de1f00d2906e2374a9.chromatic.com/',
+    },
+  },
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
